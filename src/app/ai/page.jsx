42c62452e55f19ff0209dcb67dd8621d98f58f19
farel <farelnanda11@gmail.com>
2024-06-14 23:57:42 +0700

    update

--- src/app/ai/page.jsx
+++ src/app/ai/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { reqGroqAi } from "@/utils/groq";
 import { useState } from "react";
 import { Light as SyntaxHightLight } from "react-syntax-highlighter";
 
@@ -8,10 +7,11 @@ const Page = () => {
   const [message, setMessage] = useState();
 
   const handlerSubmit = async () => {
-    const ai = await reqGroqAi(content.value);
-    setMessage(ai);
-    content.value = ""
+    const response = await fetch(`${process.env.NEXT_PUBLIC_TAHUAPI_URL}/groqai/${content.value}`)
+    const data = await response.json()
+    setMessage(data)
   };
+  
   return (
     <main className="w-full min-h-screen flex justify-center items-center p-4 py-12 mt-8">
       <div className="min-h-[80vh] w-[80vh] p-4 border-2 rounded-lg shadow-[20px_20px_10px_0px_rgb(0,0,0,0.5)] flex flex-col items-center max-w-xl mx-auto">
